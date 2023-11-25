544e6187356efc58024a2e333f6fba781cae0558
Rafhael0069 <capic.2018tads0069@aluno.ifpi.edu.br>
2023-11-25 17:01:18 -0300

    :lipstick: feat: Ajustando detalhes de eventos do menu

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
 // main.js
 
+let navbar = document.querySelector('.navbar');
+let iconnav = document.querySelectorAll('.nav-item');
+let navlink = document.querySelectorAll('.nav-link');
+
 document.addEventListener("DOMContentLoaded", function () {
-    let iconnav = document.querySelectorAll('.nav-item');
-    let navlink = document.querySelectorAll('.nav-link');
-    iconnav.forEach(element => {
-        element.classList.add('m-4');
-    });
     navlink.forEach(element => element.style.setProperty('color', 'var(--secundaria)'));
 });
 
@@ -18,42 +17,45 @@ document.addEventListener("DOMContentLoaded", function () {
 
     menuToggle.addEventListener('click', function () {
         if (!state.menu) {
-            navbarCollapse.classList.toggle('show');
             state.menu = true;
+            setColoredMenu();
         } else {
-            navbarCollapse.classList.toggle('show');
             state.menu = false;
+            if (window.scrollY < 100) {
+                setTransparentMenu();
+            }
         }
     });
+
+    iconnav.forEach((item) => {
+        item.addEventListener('click', function () {
+            navbarCollapse.classList.toggle('show');
+            if(state.menu == true){
+                state.menu = false;
+                if (window.scrollY < 100) {
+                    setTransparentMenu();
+                }
+            }
+        });
+    })
 });
 
 window.addEventListener("scroll", function () {
-    let navbar = document.querySelector('.navbar');
-    let iconnav = document.querySelectorAll('.nav-item');
-    let navlink = document.querySelectorAll('.nav-link');
-
-    
-    if (window.scrollY === 0) {
-        iconnav.forEach(element => {
-            //element.classList.remove('mx-1');
-            //element.classList.add('m-4');
-            element.classList.remove('m-4');
-            element.classList.add('mx-1');
-        });
-    } else {
-        iconnav.forEach(element => {
-            element.classList.remove('m-4');
-            element.classList.add('mx-1');
-        });
-    }
-
     if (window.scrollY < 100) {
-        navbar.style.setProperty('background-color', 'transparent', 'important');
-        iconnav.forEach(element => element.style.setProperty('color', 'var(--secundaria)'));
-        navlink.forEach(element => element.style.setProperty('color', 'var(--secundaria)'));
+        setTransparentMenu();
     } else {
-        navbar.style.setProperty('background-color', 'var(--secundaria)', 'important');
-        iconnav.forEach(element => element.style.setProperty('color', 'var(--secundaria-clara)'));
-        navlink.forEach(element => element.style.setProperty('color', 'var(--secundaria-clara)'));
+       setColoredMenu();
     }
 });
+
+function setColoredMenu() {
+    navbar.style.setProperty('background-color', 'var(--secundaria)', 'important');
+    iconnav.forEach(element => element.style.setProperty('color', 'var(--secundaria-clara)'));
+    navlink.forEach(element => element.style.setProperty('color', 'var(--secundaria-clara)'));
+}
+
+function setTransparentMenu() {
+    navbar.style.setProperty('background-color', 'transparent', 'important');
+    iconnav.forEach(element => element.style.setProperty('color', 'var(--secundaria)'));
+    navlink.forEach(element => element.style.setProperty('color', 'var(--secundaria)'));
+}
